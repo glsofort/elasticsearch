fae43663d6d9f14110fee3d0f60c09be7aefeb50
namxle <namxle3010@gmail.com>
2023-12-12 15:55:55 +0700

    feat: build image script

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-docker build -t registry.cn-shenzhen.aliyuncs.com/gls-production/elasticsearch:1.0.0 .
+# image_export: registry.cn-shenzhen.aliyuncs.com/gls-production/elasticsearch:1.0.0
 
-docker run --rm --memory="4g" --name es01 --net elastic -p 9200:9200 -itd  gls-elasticsearch:8.11.1
+docker stop es01
+
+docker build -t build-gls-elasticsearch:1.0.0 .
+
+docker run --rm --memory="4g" --name es01 --net elastic -p 9200:9200 -itd  build-gls-elasticsearch:8.11.1
 
 docker cp es01:/usr/share/elasticsearch/config/certs/http_ca.crt .
 
