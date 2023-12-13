80351b2a437cf9f0dfac4f0843a9d4d9f1fbccd2
namxle <103374580+namxle@users.noreply.github.com>
2023-12-13 14:12:02 +0700

    feat: change elastisearch version to 8.11.0

--- build-image.sh
+++ build-image.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# image_export: registry.cn-shenzhen.aliyuncs.com/gls-production/elasticsearch:1.0.0
+# image_export: registry.cn-shenzhen.aliyuncs.com/gls-production/elasticsearch:8.11.0
 
 docker stop es01
 
-docker build -t build-gls-elasticsearch:8.11.1 .
+docker build -t build-gls-elasticsearch:8.11.0 .
 
 docker rm es01
 
-docker run --rm --memory="4g" --name es01 --net elastic -p 9200:9200 -itd  build-gls-elasticsearch:8.11.1
+docker run --rm --memory="4g" --name es01 --net elastic -p 9200:9200 -itd  build-gls-elasticsearch:8.11.0
 
 docker cp es01:/usr/share/elasticsearch/config/certs/http_ca.crt .
 
-# Qdcfx*fM8YlpUxg4Qwb9
+# aPF4a30LlAUcPPx5TVVy
